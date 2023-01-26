729f4f2a620fb7638034ac3c00e1b4bc21190745
vitor-fontenele <vitor_fontenele@hotmail.com>
2023-01-26 10:55:53 -0300

    Update exercise

--- src/index.ts
+++ src/index.ts
@@ -289,57 +289,143 @@ app.get("/products", async (req: Request, res: Response) => {
 })
 
 // Edit Product by Id
-app.put("/product/:id", (req: Request, res: Response) => {
+app.put("/product/:id", async (req: Request, res: Response) => {
     try {
-        const id = req.params.id;
-        const name = req.body.name;
-        const price = req.body.price;
-        const category = req.body.category;
+        // path params
+        const idToEdit = req.params.id;
+        
+        // body params
+        const newId = req.body.id;
+        const newName = req.body.name;
+        const newPrice = req.body.price;
+        const newDescription = req.body.description;
+        const newImageUrl = req.body.imageUrl;
+        const newCategory = req.body.category;
 
-        const product = products.find(product => product.id === id);
+        // idToEdit é obrigatoriamente uma string, pois é path param
 
-        if (!product){
-            res.status(404);
-            throw new Error ("Produto não encontrado");
-        }
-
-        if (name !== undefined){
-            if (typeof name !== "string"){
+        if (newId !== undefined){
+            // newId se definido precisa ser uma string
+            if (typeof newId !== "string"){
                 res.status(400);
-                throw new Error ("Nome do produto deve ser uma string");
+                throw new Error ("'id' precisa ser uma string");
+            }
+            // newId precisa ser diferente dos que já existem
+            const newIdExists = await db("products").where({id: newId});
+            if (newIdExists.length > 0){
+                res.status(400);
+                throw new Error ("Já existe um 'product' com esse 'id'");
+            }
+            // id do product precisa ter no mínimo 7 caracteres
+            if (newId.length < 7){
+                res.status(400);
+                throw new Error ("'id' precisa ter no mínimo 7 caracteres");
             }
         }
 
-        if (price !== undefined){
-            if (typeof price !== "number"){
+        if (newName !== undefined){
+            // newName precisa ser uma string
+            if (typeof newName !== "string"){
                 res.status(400);
-                throw new Error ("Preço do produto deve ser um número");
+                throw new Error ("'name' precisa ser uma string");
+            }
+            // name precisa ter no mínimo 2 caracteres
+            if (newName.length < 2){
+                res.status(400);
+                throw new Error ("'name' precisa ter no mínimo 2 caracteres");
             }
         }
 
-        if (category !== undefined){
+        if (newPrice !== undefined){
+            // price precisa existir e ser um number
+            if (typeof newPrice !== "number"){
+                res.status(400);
+                throw new Error ("'price' deve ser um number");
+            }
+            // newPrice precisa ser maior do que zero
+            if (newPrice <= 0){
+                res.status(400);
+                throw new Error ("'price' deve ser maior do que zero");
+            }
+        }
+
+        if (newDescription !== undefined){
+            // newDescription precisa ser uma string
+            if (typeof newDescription !== "string"){
+                res.status(400);
+                throw new Error ("'description' ser uma string");
+            }
+            // newDescription precisa ter no mínimo 2 caracteres
+            if (newDescription.length < 2){
+                res.status(400);
+                throw new Error ("'description' deve ter no mínimo 2 caracteres");
+            }
+        }
+
+        if (newImageUrl !== undefined){
+            // newImageUrl precisa existir e ser uma string
+            if (typeof newImageUrl !== "string"){
+                res.status(400);
+                throw new Error ("'imageUrl' deve ser uma string");
+            }
+            // URLs podem ter muitos formatos. Fica pendente ver se vale alguma outra verificação
+        }
+
+        if (newCategory !== undefined){
+            // newCategory deve ser uma string
+            if (typeof newCategory !== "string"){
+                res.status(400);
+                throw new Error ("'category' deve ser uma string");
+            }
+            // newCategory precisa ser uma das categories existentes
             if (
-                category !== "Acessórios" &&
-                category !== "Roupas" &&
-                category !== "Eletrônicos"
+                newCategory !== "Acessories" &&
+                newCategory !== "Clothes and Shoes" &&
+                newCategory !== "Electronics"
             ){
                 res.status(400);
-                throw new Error ("Categoria deve ser uma das existentes");
+                throw new Error ("Produto deve ter uma categoria existente");
             }
         }
 
-        // Atualizando os dados
-        product.name = name || product.name;
-        product.price = price || product.price;
-        product.category = category || product.category;
+        // produto a ser editado
+        const [productToEdit] = await db("products").where({id: idToEdit});
 
-        res.status(200).send("Produto atualizado com sucesso!");
+        // caso não haja um produto com id === idToEdit
+        if (!productToEdit){
+            res.status(404);
+            throw new Error ("Não foi encontrado um produto existente com id indicado para ser editado");
+        }
+
+        // produto atualizado
+        const updatedProduct = {
+            id: newId || productToEdit.id,
+            name: newName || productToEdit.name,
+            price: newPrice || productToEdit.price,
+            description: newDescription || productToEdit.description,
+            imageUrl: newImageUrl || productToEdit.imageUrl,
+            category: newCategory || productToEdit.category
+        }
+
+        // query para atualizar o produto
+        await db("products").update(updatedProduct).where({id: idToEdit});
+
+        res.status(200).send({
+            message: "Produto atualizado com sucesso!",
+            product: updatedProduct
+        });
     } catch (error) {
-        console.log(error)
-        if (res.statusCode === 200){
+        console.log(error);
+
+        if (req.statusCode === 200) {
             res.status(500);
         }
-        res.send(error.message);
+
+        if (error instanceof Error) {
+            res.send(error.message);
+        } else {
+            res.send("Erro inesperado");
+        }
     }
 });
 
